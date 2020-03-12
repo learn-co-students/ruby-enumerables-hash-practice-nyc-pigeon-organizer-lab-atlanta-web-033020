27471ab8ac6fe14fd24df295e3c627fd09510743
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-12 19:56:21 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  hash = {}
+  
+  data.each do |key, value|
+    value.each do |next_key, names|
+      names.each do |name|
+        if !hash[name]
+          hash[name] = {}
+        end
+        
+        if !hash[name][key]
+          hash[name][key] = []
+        end
+        
+        hash[name][key].push(next_key.to_s)
+        
+      end
+    end
+  end
+  
+  return hash
 end
