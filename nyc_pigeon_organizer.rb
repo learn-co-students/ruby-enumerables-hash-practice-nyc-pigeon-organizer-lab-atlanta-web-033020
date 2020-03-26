d9c45feaa4db1dff503a6594bebd2d30bbfcb69b
Cory Rosser <coryrosser@gmail.com>
2020-03-26 18:49:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |att, name_array|
+      name_array.each do |name| 
+        if !new_hash.include? name
+          new_hash[name] = {}
+        end 
+        if !new_hash[name].include? key 
+          new_hash[name][key] = []
+        else
+      end
+      new_hash[name][key] << att.to_s 
+    end
+  end
+end
+new_hash
 end
