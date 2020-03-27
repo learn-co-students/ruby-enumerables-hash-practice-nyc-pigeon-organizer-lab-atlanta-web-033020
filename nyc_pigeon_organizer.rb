565c1a398a2a0f7bbec7b0afbf1981e7627748e6
Geoffrey Harrison <harrisongeo87@gmail.com>
2020-03-27 04:48:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,51 @@
+def build_structure(name_array)
+  new_hash = Hash.new
+  name_array.each do |name| 
+    new_hash[name] = {
+      color: [],
+      gender: [],
+      lives: []
+    }
+  end
+  new_hash
+end
+
+def loop_and_add(data, name)
+  new_array = []
+  key_array = data.keys
+  key_array.each do |key|
+    new_array << key.to_s if data[key].include?(name)
+  end
+  new_array
+end
+
+def add_data_to_name(old_data, new_data, name)
+  new_hash = new_data
+  
+  new_hash[:color] = loop_and_add(old_data[:color], name)
+  new_hash[:gender] = loop_and_add(old_data[:gender], name)
+  new_hash[:lives] = loop_and_add(old_data[:lives], name)
+  
+  new_hash
+end
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  
+  name_keys = []
+  data[:gender][:male].each { |name| name_keys << name }
+  data[:gender][:female].each { |name| name_keys << name }
+  
+  pidgeon_names = build_structure(name_keys)
+  
+  name_keys.each do |name|
+    #pp name
+    name_data = add_data_to_name(data, pidgeon_names[name], name)
+    #pp name_data
+    pidgeon_names[name] = name_data
+    #pp pidgeon_names[name]
+    #puts " "
+  end
+  
+  pidgeon_names
 end
